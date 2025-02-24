5c2df56346135660b6a6bfccc0536f198a9e958b
Pal Gajera <palgajera7@gmail.com>
2025-02-24 20:27:13 +0530

    Add files via upload

---
+++ Assigment/Module 1/Lab/EX2.py
@@ -0,0 +1,10 @@
+
+age = 25              # Integer  
+price = 99.99         # Float  
+name = "Pal"         # String  
+is_student = True     # Boolean  
+fruits = ["Apple", "Banana"]  # List  
+person = {"name": "Pal", "age": 25}  # Dictionary  
+
+
+print(age, price, name, is_student, fruits, person)
