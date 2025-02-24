867f8b7b5d937dd66f8c24f8bc21f46c7583cfda
Pal Gajera <palgajera7@gmail.com>
2025-02-24 20:24:36 +0530

    Add files via upload

---
+++ Assigment/Module 1/Lab/EX5.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Number 1 : "))
+number2 = int(input("Enter Number 2 : "))
+
+
+if(number>number2):
+    print("Number 1 is greater")
+else:
+    print("Number 2 is greater")
