d92f3fe72fe75051ec9c5fbec6414ee191fb5570
Pal Gajera <palgajera7@gmail.com>
2025-05-27 13:10:54 +0530

    Delete Module 4 - DB and Framework directory

--- Module 4 - DB and Framework/Django Lab/Eshop/shop/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class productForm(forms.ModelForm):
-    class Meta:
-        model=product_detail
-        fields=['pname','qty','price','pimage']
--- Module 4 - DB and Framework/Django Lab/database/appname/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import StudInfo
-
-class StudInfoForm(forms.ModelForm):
-    class Meta:
-        model = StudInfo
-        fields = "__all__"
